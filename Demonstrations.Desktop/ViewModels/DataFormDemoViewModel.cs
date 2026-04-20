161b83c6e735bb7958d269ce8f0a6e14c1ae1329
MrDeef <aleksanrovich211@mail.ru>
2026-04-20 12:47:48 +0700

    Update DataFormDemoViewModel.cs

--- Demonstrations.Desktop/ViewModels/DataFormDemoViewModel.cs
+++ Demonstrations.Desktop/ViewModels/DataFormDemoViewModel.cs
@@ -251,7 +251,8 @@ namespace Demonstrations.Desktop.ViewModels
             config.SetFieldRule("Phone", new DataFormFieldConfig
             {
                 Order = 2,
-                RowGroup = 1
+                RowGroup = 1,
+                HideLabel = true
             });
 
             config.SetCategoryOrder("Основные данные", 1);
